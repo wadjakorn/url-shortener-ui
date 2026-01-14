4af56afabb2ae7ec43011936c76530e0195cf8c5
wadjakorn.t <wazjakorn@gmail.com>
2026-01-14 22:33:56 +0700

    fix: redirect config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
